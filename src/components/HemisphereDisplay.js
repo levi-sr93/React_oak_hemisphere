4fe055f500d0a775f6361e8d1044beb46d3ff1bf
levi-sr93 <levi-sr@outlook.com>
2020-09-24 15:44:41 -0300

    Show image based on the location

--- src/components/HemisphereDisplay.js
+++ src/components/HemisphereDisplay.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import northPic from '../img/north.jpg';
+import southPic from '../img/south.jpg';
+
+
 const HemisphereDisplay = ({latitude}) => {
   console.log(latitude)
   const hemisphere = latitude > 0 ? 'Nothern Hemisphere' : 'Southern Hemisphere';
+  const picture = latitude > 0 ? northPic : southPic;
+
   return (
     <div>
+      <img src={picture} alt="Hemisphere" style={{width:"300px"}}/>
       {hemisphere}
     </div>
   )
