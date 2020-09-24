f88cb2134ced5675be66804030b38ba8d775b55f
Levi Rocha <levi.rocha@teclat.com.br>
2020-09-24 11:13:49 -0300

    Basic Structure and Getting User Location

---
+++ src/components/HemisphereDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HemisphereDisplay = () => {
+  return (
+    <div>
+      Hey you are in NOrthern Hemisphere
+    </div>
+  )
+}
+
+export default HemisphereDisplay;
