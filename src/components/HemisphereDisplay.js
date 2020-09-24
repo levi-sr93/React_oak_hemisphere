b0b0558887674de93d8eaaa67edde11a05493dd2
levi-sr93 <levi-sr@outlook.com>
2020-09-24 17:00:22 -0300

    Styling

--- src/components/HemisphereDisplay.js
+++ src/components/HemisphereDisplay.js
@@ -1,18 +1,35 @@
 import React from 'react';
+import './Hemisphere.css'
 
 import northPic from '../img/north.jpg';
 import southPic from '../img/south.jpg';
 
+const hemisphereConfig = {
+  Northern: {
+    text: 'it is northern hemisphere',
+    picture: northPic
+  },
+  Southern: {
+    text: 'it is southern hemisphere',
+    picture: southPic
+  }
+}
 
 const HemisphereDisplay = ({latitude}) => {
   console.log(latitude)
-  const hemisphere = latitude > 0 ? 'Nothern Hemisphere' : 'Southern Hemisphere';
-  const picture = latitude > 0 ? northPic : southPic;
+  const hemisphere = latitude > 0 ? 'Northern' : 'Southern';
+  const {text, picture} = hemisphereConfig[hemisphere]
 
   return (
-    <div>
-      <img src={picture} alt="Hemisphere" style={{width:"300px"}}/>
-      {hemisphere}
+    <div className={hemisphere}>
+      <div className="ui raised text container segment">
+        <div className="image">
+          <img src={picture} alt="Hemisphere"/>
+        </div>
+        <div className="ui teal bottom attached label">
+          <h1>{text}</h1>
+        </div>
+      </div>
     </div>
   )
 }
